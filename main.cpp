2f96960b2c97096a94bbe23c1b5b59ed798a97ba
Akshay Chandrachood <akshay.chandrachood@gmail.com>
2017-10-02 23:23:45 -0500

    cancer category added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,11 @@
 #include <string.h>
 #include "data_structures.h"
 
+#define var_list_size 19
+#define clause_list_size 41
+#define conc_list_size 10
+
+
 using namespace std;
 
 void initialize_lists();
@@ -14,17 +19,17 @@ void evaluate_then_part(int rule);
 void check_var_list(string var_to_check);
 void iterate(int);
 
-ConclusionList conc_list[7];
-VariableList var_list[11];
+ConclusionList conc_list[conc_list_size];
+VariableList var_list[var_list_size];
 NewRules rules[4];
-ClauseVarList clause_vars[29];
+ClauseVarList clause_vars[clause_list_size];
 bool rule_found();
 
 stack<int> cn_stack;
 stack<int> sn_stack;
 
 int sn,i;
-int cn;
+int cn,count;
 int pos;
 int var_in_clause_list = 1;
 string conclusion;
@@ -48,21 +53,27 @@ int main()
     pos=0;  //pos is the index of conc_list
 
         sn = conclusion_search(pos,conclusion);
-
         if(sn==-1){
             cout<<"Conclusion Not Found.."<<endl;
             return 0;
         }
 
-
         else
             backward_chaining(sn, conclusion);
 
-            while(conc_list[pos].get_conclusion_value()=="" && pos<7){
-                pos++;sn_stack.pop(); cn_stack.pop();
-                sn = conclusion_search(pos,conclusion);
-                backward_chaining(sn, conclusion);
-            }
+          for(int i=0;i<conc_list_size;i++){
+             if(conc_list[i].get_rule()==sn){
+                pos = i; break;
+             }
+          }
+        cout<<"pos = "<<pos<<endl;
+        while(conc_list[pos].get_conclusion_value()=="" && pos<conc_list_size){
+              pos++;sn_stack.pop(); cn_stack.pop();
+              sn = conclusion_search(pos,conclusion);
+              if(sn!=-1)
+                 backward_chaining(sn, conclusion);
+        }
+
 
      while(!sn_stack.empty()){
         int con_no;
@@ -74,6 +85,9 @@ int main()
              case 50: con_no = 4; break;
              case 60: con_no = 5; break;
              case 70: con_no = 6; break;
+             case 80: con_no = 7; break;
+             case 90: con_no = 8; break;
+             case 100: con_no = 9; break;
         }
         if(conc_list[con_no].get_conclusion_value()!=""){
             cout<<endl<<"********************************************************"<<endl;
@@ -117,7 +131,7 @@ void backward_chaining(int sn, string conclusion)
 
 void check_var_list(string var_to_check)
 {
-    for(int i=0;i<11;i++)
+    for(int i=0;i<var_list_size;i++)
     {
         if(var_to_check == var_list[i].getVarName())
         {
@@ -135,7 +149,7 @@ void check_var_list(string var_to_check)
 
 int conclusion_search(int pos, string var)
 {
-    for(int i=pos;i<7;i++)
+    for(int i=pos;i<conc_list_size;i++)
     {
         if(var == conc_list[i].get_conclusion())
         {
@@ -154,7 +168,7 @@ void evaluate_then_part(int rule)
                         {
                             conc_list[0].set_value("yes");
                             case_no = 0;
-                        }else{conc_list[0].set_value("no");};
+                        }else{conc_list[0].set_value("no");}
                         break;
 
             case 20:   if(conc_list[0].get_conclusion_value() == "yes" &&  var_list[3].getValue() == "yes")
@@ -191,13 +205,38 @@ void evaluate_then_part(int rule)
                             case_no = 5;
                         }
                         break;
-
-             case 70:    if(var_list[8].getValue() == "yes" && var_list[9].getValue() == "yes" && var_list[10].getValue() == "yes")
+            case 70:    if(var_list[8].getValue() == "yes" && var_list[9].getValue() == "yes" && var_list[10].getValue() == "yes" && var_list[11].getValue() == "yes")
                         {
-                            conc_list[6].set_value("cns_lymphonia");
+                            conc_list[6].set_value("nodular");
                             case_no = 6;
                         }
                         break;
+
+            case 80:    cout<<"In case 7 "<<endl;
+                        if(var_list[8].getValue() == "no" && var_list[12].getValue() == "yes" && var_list[13].getValue() == "yes" && var_list[14].getValue() == "yes")
+                        {
+                            conc_list[7].set_value("acral_lentigious");
+                            case_no = 7;
+                        }
+                        break;
+
+            case 90:    cout<<"In case 8 "<<endl;
+                        if(var_list[8].getValue() == "no" && var_list[15].getValue() == "yes" && var_list[16].getValue() == "yes")
+                        {
+                            conc_list[8].set_value("lentigo");
+                            case_no = 8;
+                        }
+                        break;
+
+             case 100:    cout<<"In case 10 "<<endl;
+                        if(var_list[8].getValue() == "yes" && var_list[9].getValue() == "no"
+                           && var_list[17].getValue() == "yes" && var_list[18].getValue() == "yes")
+                        {
+                            conc_list[9].set_value("super_facial_spreading");
+                            case_no = 9;
+                        }
+                        break;
+
         }
 }
 
@@ -217,30 +256,23 @@ void initialize_lists()
     var_list[6].init(0,"chest_pain","");
     var_list[7].init(0,"fatigue","");
 
-    //BRAIN CANCER
-    var_list[8].init(0,"head_ache","");
-    var_list[9].init(0,"vision_loss","");
-    var_list[10].init(0,"partial_paralysis","");
- /*   var_list[12].init(0,"migrain","");
-    var_list[13].init(0,"lack_of_coordination","");
-    var_list[14].init(0,"personality_changes","");
-    var_list[15].init(0,"speech_disorder","");
-    var_list[16].init(0,"depression","");
-    var_list[17].init(0,"hearing_loss","");
-    var_list[18].init(0,"tinnitus",""); */
+    var_list[8].init(0,"itching","");
+    var_list[9].init(0,"head_neck_affected","");
+    var_list[10].init(0,"done_shaped_lump","");
+    var_list[11].init(0,"bleeding","");
+    var_list[12].init(0,"nails_affected","");
+    var_list[13].init(0,"border_irregularity","");
+    var_list[14].init(0,"assymetry","");
+    var_list[15].init(0,"moles>6mm","");
+    var_list[16].init(0,"face_affected","");
 
-
-    //var_list[10].init(0,"partial_paralysis","");
-
+    var_list[17].init(0,"moles_beyond_border","");
+    var_list[18].init(0,"variable_pigmentation","");
 
 
 
 
-
-//    var_list[11].init(0,"lack_of_coordination","");
-//    var_list[12].init(0,"personality_changes","");
-
-    for(int i = 0 ; i < 11;i++)
+    for(int i = 0 ; i < 15;i++)
     {
 		var_list[i].printVarList();
 		cout<<endl;
@@ -256,13 +288,14 @@ void initialize_lists()
     conc_list[3].set_rule(40,"lung_cancer");
     conc_list[4].set_rule(50,"lung_cancer");
     conc_list[5].set_rule(60,"lung_cancer");
-     //Conclusions for Lung Cancer
-    conc_list[6].set_rule(70,"brain_cancer");
-//    conc_list[7].set_rule(80,"migrain");
-//    conc_list[8].set_rule(90,"brain_cancer");
-//    conc_list[9].set_rule(100,"brain_cancer");
 
-    for(int i=0;i<7;i++)
+    conc_list[6].set_rule(70,"skin_cancer_itching");
+    conc_list[7].set_rule(80,"skin_cancer");
+    conc_list[8].set_rule(90,"skin_cancer");
+    conc_list[9].set_rule(100,"skin_cancer_itching");
+    conc_list[10].set_rule(110,"skin_cancer");
+
+    for(int i=0;i<10;i++)
     {
         conc_list[i].print_rule();
         cout<<"\n";
@@ -277,12 +310,7 @@ void initialize_lists()
     rules[2].set_then_part(30, "lung_cancer", "yes");
     rules[3].set_if_part2(40, "wheezing","no", "achiness_back_shoulder", "yes");
     rules[3].set_then_part(40, "lung_cancer", "yes");
-    for(int i = 0 ; i < 4;i++)
-    {
-		rules[i].print_rules();
-		cout<<endl;
-	}
-    cout<<endl;
+
 
     cout<<"Initializing Clause Variables.."<<endl;
     clause_vars[0].set_vars(0,"");
@@ -311,15 +339,30 @@ void initialize_lists()
     clause_vars[23].set_vars(23,"cough_blood");
     clause_vars[24].set_vars(24,"");
 
-    //brain cancer
-    clause_vars[25].set_vars(25,"head_ache");
-    clause_vars[26].set_vars(26,"vision_loss");
-    clause_vars[27].set_vars(27,"partial_paralysis");
-    clause_vars[28].set_vars(28,"");
- //   clause_vars[20].set_vars(20,"fatigue");
+    clause_vars[25].set_vars(25,"itching");
+    clause_vars[26].set_vars(26,"head_neck_affected");
+    clause_vars[27].set_vars(27,"done_shaped_lump");
+    clause_vars[28].set_vars(28,"bleeding");
+
+    clause_vars[29].set_vars(29,"itching");
+    clause_vars[30].set_vars(30,"nails_affected");
+    clause_vars[31].set_vars(31,"border_irregularity");
+    clause_vars[32].set_vars(32,"assymetry");
+
+    clause_vars[33].set_vars(33,"itching");
+    clause_vars[34].set_vars(34,"moles>6mm");
+    clause_vars[35].set_vars(35,"face_affected");
+    clause_vars[36].set_vars(36,"");
+
+    clause_vars[37].set_vars(37,"itching");
+    clause_vars[38].set_vars(38,"moles_beyond_border");
+    clause_vars[39].set_vars(39,"head_neck_affected");
+    clause_vars[40].set_vars(40,"variable_pigmentation");
 
 
-    for(int i = 0 ; i <29;i++)
+
+
+    for(int i = 0 ; i <41;i++)
     {
 		clause_vars[i].print_clause_vars();
 		cout<<endl;
